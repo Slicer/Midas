Slicer/Midas

commit 9a2a3b81cb3abf00e1e7ab1212acee2f69ed103f

Add parameters to fix the MIDAS API display
Add a set of parameters which will limit the fields that are shown
when a user accesses the listing of public users.  One side is the field
values that we want to capture information for, the other is to unset
the values that are not queried from the model before it is returned.

Cherry-pick by @jcfr from https://github.com/josephsnyder/Midas/commit/904acb0b309f5f31e633959062acaaf50e049c0e

core/controllers/components/ApiuserComponent.php
@@ -55,7 +55,7 @@ class ApiuserComponent extends AppComponent
     $apihelperComponent->validateParams($args, array('limit'));
 
     $userModel = MidasLoader::loadModel('User');
-    return $userModel->getAll(true, $args['limit']);
+    return $userModel->getAll(true, $args['limit'], array('firstname','lastname','company','website'));
     }
 
   /**

core/models/pdo/UserModel.php
@@ -93,9 +93,10 @@ class UserModel extends UserModelBase
     } // end getUserCommunities
 
   /** Get all */
-  function getAll($onlyPublic = false, $limit = 20, $order = 'lastname', $offset = null, $currentUser = null)
+  function getAll($onlyPublic = false, $limit = 20, $fields = array('*'), $order = 'lastname', $offset = null, $currentUser = null)
     {
     $sql = $this->database->select();
+    $sql->from("user",$fields);
     if($onlyPublic)
       {
       $orClause = '';
@@ -138,7 +139,7 @@ class UserModel extends UserModelBase
     $return = array();
     foreach($rowset as $row)
       {
-      $return[] = $this->initDao('User', $row);
+      $return[] = $this->initDao('User', $row, null, true);
       }
     return $return;
     } // end getAll()

core/models/MIDASModel.php
@@ -160,7 +160,7 @@ class MIDASModel
    * @param $name name of the dao
    * @param $data array of values
    */
-  public function initDao($name, $data, $module = null)
+  public function initDao($name, $data, $module = null, $removeOld = false)
     {
     // If no data found we return false
     if(!$data)
@@ -198,6 +198,13 @@ class MIDASModel
         if(isset($data[$name]))
           {
           $obj->$name = $data[$name];
+          }
+          else
+          {
+            if ($removeOld)
+              {
+              unset($model->_mainData[$name]);
+              }
           }
         }
       $obj->saved = true;

commit fd959c06448ebc8e79914a037acb2075f9e9f846

Fix regression causing UserModel error. See midasplatform/Midas#265
This commit fixes a regression introduced in 9a2a3b8 (Add parameters
to fix the MIDAS API display)

core/controllers/components/ApiuserComponent.php
@@ -54,8 +54,16 @@ class ApiuserComponent extends AppComponent
     $apihelperComponent = MidasLoader::loadComponent('Apihelper');
     $apihelperComponent->validateParams($args, array('limit'));
 
+
     $userModel = MidasLoader::loadModel('User');
-    return $userModel->getAll(true, $args['limit'], array('firstname','lastname','company','website'));
+    return $userModel->getAll(
+      /* $onlyPublic = */ true,
+      /* $limit = */ $args['limit'],
+      /* $order = */ 'lastname',
+      /* $offset = */ null,
+      /* $currentUser = */ null,
+      /* $fields = */ array('firstname','lastname','company','website')
+      );
     }
 
   /**

core/models/pdo/UserModel.php
@@ -93,7 +93,7 @@ class UserModel extends UserModelBase
     } // end getUserCommunities
 
   /** Get all */
-  function getAll($onlyPublic = false, $limit = 20, $fields = array('*'), $order = 'lastname', $offset = null, $currentUser = null)
+  function getAll($onlyPublic = false, $limit = 20, $order = 'lastname', $offset = null, $currentUser = null, $fields = array('*'))
     {
     $sql = $this->database->select();
     $sql->from("user",$fields);